45386c93a19a5ce742d107c62476d256b26651b4
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2021-08-27 23:21:35 +0545

    system based dark/light theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'page/HomePage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -3,17 +3,28 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("This is app bar"),
-      ),
-      drawer: Drawer(
-        child: Center(
-          child: Text("Drawer"),
+    return MaterialApp(
+      themeMode: ThemeMode.system, // auto dark mode
+      // configs to apply when dark mode
+      // theme: named arg for light theme config
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          color: Colors.deepOrangeAccent,
         ),
       ),
-      body: Center(
-        child: Text("This is body"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("This is app bar"),
+        ),
+        drawer: Drawer(
+          child: Center(
+            child: Text("Drawer"),
+          ),
+        ),
+        body: Center(
+          child: Text("This is body"),
+        ),
       ),
     );
   }
